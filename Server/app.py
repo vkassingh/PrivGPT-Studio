b4fe39b4a9e51e5f980b829ca9593c3cc5be1b9c
Rucha Ambaliya <Rambaliyaforall@gmail.com>
2025-10-07 16:46:53 +0530

    Fix CORS

--- Server/app.py
+++ Server/app.py
@@ -1,14 +1,20 @@
 import os
 import google.generativeai as genai
 import sys
+from Server import create_app
+from flask_cors import CORS
+
 # Add parent directory to Python path to allow Server module import
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from Server import create_app
+
 # Initialize Flask app
 app = create_app()
+CORS(app, origins=["https://privgpt-studio.vercel.app", "http://127.0.0.1:5000"])
+
 # list available models
 for m in genai.list_models():
     print(m.name, m.supported_generation_methods)
+    
 # start server
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
